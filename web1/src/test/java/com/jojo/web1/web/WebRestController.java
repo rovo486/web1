82e72a4d58a6f62a7a41a6167c2993d2d947cf42
rovo486 <rovo486@gmail.com>
2020-02-24 10:50:28 +0900

    web1 0224 1

---
+++ web1/src/test/java/com/jojo/web1/web/WebRestController.java
@@ -0,0 +1,16 @@
+package com.jojo.web1.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+
+	@GetMapping("/hello")
+    public String hello() {  
+        return "HelloWorld12131";
+    }
+	 
+
+}
